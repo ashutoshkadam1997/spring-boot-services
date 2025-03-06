c742e79cb44981c0dc7fec0dd1f7f06aafd56bde
ashutoshkadam1997 <ashutosh.kadam1997@gmail.com>
2025-03-06 20:13:44 +0530

    Spring security project first commit

---
+++ learn-spring-security/src/main/java/dev/ashutosh/sc/learnSpringSecurity/LearnSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.ashutosh.sc.learnSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ learn-spring-security/src/test/java/dev/ashutosh/sc/learnSpringSecurity/LearnSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ashutosh.sc.learnSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
